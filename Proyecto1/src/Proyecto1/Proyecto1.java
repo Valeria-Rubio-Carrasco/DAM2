8a1797368c3fe39f1e86ce49f9cb3922fdf3daf6
ALUMNO2425 <ALUMNO2425@DESKTOP-RN8QO6K>
2025-04-07 14:40:18 +0200

    Primer control de versiones con GIT

---
+++ Proyecto1/src/Proyecto1/Proyecto1.java
@@ -0,0 +1,12 @@
+package Proyecto1;
+
+public class Proyecto1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Estamos haciendo control de versiones con GIT");
+		System.out.println("Estamos haciendo el segundo control de versiones con GIT");
+	}
+	
+
+}
