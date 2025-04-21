df0344f1b01b7a93edfb0a1f1a98e59cc5116108
ALUMNO2425 <ALUMNO2425@DESKTOP-RN8QO6K>
2025-04-21 14:11:42 +0200

    Subido

--- Proyecto1/src/Proyecto1/Proyecto1.java
+++ Proyecto1/src/Proyecto1/Proyecto1.java
@@ -7,6 +7,8 @@ public class Proyecto1 {
 		System.out.println("Estamos haciendo control de versiones con GIT");
 		System.out.println("Estamos haciendo el segundo control de versiones con GIT");
 		System.out.println("Estamos haciendo el tercer control de versiones con GIT");
+		System.out.println("Hoy es 21/04/2025");
+		
 	}
 	
 
