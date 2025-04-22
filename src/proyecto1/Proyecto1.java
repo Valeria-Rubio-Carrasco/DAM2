c45f8435a59aaa298e39569c5ec1cf6cca225960
ALUMNO2425 <ALUMNO2425@DESKTOP-RN8QO6K>
2025-04-22 12:23:44 +0200

    Tercer commit

--- src/proyecto1/Proyecto1.java
+++ src/proyecto1/Proyecto1.java
@@ -5,9 +5,10 @@ public class Proyecto1 {
 
 
     public static void main(String[] args) {
-        System.out.print("Hola");
-        System.out.print("Hola y adiós");
-        System.out.print("Hola y adiós2");
+        System.out.print("Commit 1");
+        System.out.print("Commit 2");
+        System.out.print("Commit 3");
+        System.out.print("Commit 4");
     }
     
 }
